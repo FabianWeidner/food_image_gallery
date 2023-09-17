c77a7f5c3467f0ee00219f599468abca38fbc71f
fabian <fabian@grapes-app.com>
2023-09-17 14:22:03 +0200

    responsive image optimazation

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -1,7 +1,6 @@
-import fetchImages from "@/lib/fetchImages";
-import type { ImagesResults } from "@/models/Images";
-
-import React from 'react'
+import fetchImages from "@/lib/fetchImages"
+import type { ImagesResults } from "@/models/Images"
+import ImgContainer from "./ImgContainer"
 
 export default async function Gallery() {
 
@@ -14,7 +13,9 @@ export default async function Gallery() {
     <section className="px-2 my-3 grid gap-2 grid-cols-gallery">
 
       {images.photos.map(photo => (
-        <div key={photo.id} className="h-64 bg-gray-200 rounded-xl"></div>
+        <div key={photo.id} className="h-64 bg-gray-200 rounded-xl">
+          <ImgContainer photo={photo} />
+        </div>
       ))}
 
     </section>
